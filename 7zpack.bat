40f5159a81d08f45bfd70f98ba5265a1f76a0ae7
JM-KANE <chenxykl@outlook.com>
2021-03-31 21:01:34 +0800

    Modify

--- 7zpack.bat
+++ 7zpack.bat
@@ -1,1 +1,1 @@
-7z a MAME.7z artwork\ bgfx\ ctrlr\ docs\ hash\ hlsl\ ini\ language\ plugins\ roms\ samples\ COPYING uismall.bdf *.exe
+7z a MAME.7z artwork\ bgfx\ ctrlr\ docs\legal\ docs\man\ docs\swlist\ hash\ hlsl\ ini\ language\ plugins\ roms\ samples\ COPYING uismall.bdf *.exe
